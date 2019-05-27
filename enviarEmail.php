404245169b5c59c5c0bedad8ffc501a3bbb3ab0f
Lucas Marra <lp.marra@yahoo.com.br>
2019-05-27 11:07:10 -0300

    Update enviarEmail.php

--- enviarEmail.php
+++ enviarEmail.php
@@ -1,6 +1,11 @@
+
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
-require '../vendor/autoload.php';
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/phpmailer/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
 
 $mail = new PHPMailer();
 $mail->isSMTP();
