bf08f309fa766f32fa1ce430ba124c50267a98b7
Lucas Marra <lp.marra@yahoo.com.br>
2019-05-27 10:59:30 -0300

    PHPMailer

---
+++ enviarEmail.php
@@ -0,0 +1,34 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+require '../vendor/autoload.php';
+
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'tls';
+$mail->Username = 'marralucas1@gmail.com';
+$mail->Password = 'Candanggoplay09';
+$mail->Port = 587;
+
+//Campos
+
+$mail->setFrom('marralucas1@gmail.com');
+$mail->addReplyTo('marralucas1@gmail.com');
+$mail->addAddress('marralucas1@gmail.com', 'Nome');
+$mail->addAddress('marralucas1@gmail.com', 'Contato'’);
+
+//Msg
+
+$mail->isHTML(true);
+$mail->Subject = $nome;
+$mail->Body    = nl2br($mensagem, $numero, $email);
+$mail->AltBody = nl2br(strip_tags($mensagem, $numero, $email));
+
+//msg
+if(!$mail->send()) {
+        echo 'Não foi possível enviar a mensagem.<br>';
+        echo 'Erro: ' . $mail->ErrorInfo;
+    } else {
+         header('Location: index.php?enviado');
+    }
